6707fa8a68030246d3af877a50ecc361bcf5654c
Keeten999 <Kcsauer2004@gmail.com>
2025-09-25 15:18:24 -0600

    Hangman Actual Code
    This is the main part of the hangman Code.

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -4,12 +4,118 @@
     Purpose: A simple hangman game 
 """
 
-
-
 from random_word import RandomWords
 r = RandomWords()
 
 # Return a single random word
-r.get_random_word()
+chosen_word = r.get_random_word()
+print(chosen_word)
+incorrect_guesses = 0
+max_incorrect_guesses = 6
+guessed_letters = set()
 
-print(r.get_random_word())
+
+
+hangman_drawing = ["""
+    -----
+    |   |
+        |
+        |
+        |
+        |
+  ---------
+""",
+"""
+    -----
+    |   |
+    O   |
+        |
+        |
+        |
+  ---------
+""","""
+    -----
+    |   |
+    O   |
+    |   |
+        |
+        |
+  ---------
+""","""
+    -----
+    |   |
+    O   |
+    |\\  |
+        |
+        |
+  ---------
+""","""
+    -----
+    |   |
+    O   |
+   /|\\  |
+        |
+        |
+  ---------
+""","""
+    -----
+    |   |
+    O   |
+   /|\\  |
+   /    |
+        |
+  ---------
+""","""
+    -----
+    |   |
+    O   |
+   /|\\  |
+   / \\  |
+        |
+  ---------
+"""]
+
+print("--------------------")
+print("|  HANGMAN GAME!!  |")
+print("--------------------")
+
+while incorrect_guesses < max_incorrect_guesses:
+    display_word = ""
+    for letter in chosen_word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_"
+
+    print(hangman_drawing[incorrect_guesses])
+    print(f"Word: {display_word}")
+    print(f"Guessed Letters: {' '.join(sorted(list(guessed_letters)))}")
+    print(f"Attempts Remaining: {max_incorrect_guesses - incorrect_guesses}")
+
+    if "_" not in display_word:
+        print("You Guessed The Word!!")
+        break
+
+    guess = input("Guess: ").lower()
+
+
+    #make sure guess is one letter and numbers/ input validation
+    if not guess.isalpha() or len(guess) != 1:
+        print("Invalid input. Please enter a single letter")
+
+    #no repeats of letters
+    if guess in guessed_letters:
+        print("You already guessed that number")
+        continue
+
+    guessed_letters.add(guess)
+
+    if guess in chosen_word:
+        print(f"Good guess {guess} is in the word")
+    else:
+        print(f"Sorry, {guess} is not in the word")
+
+else:
+    print(hangman_drawing[incorrect_guesses])
+    print("GAME OVER")
+    print(f"The Word Was {chosen_word}")
